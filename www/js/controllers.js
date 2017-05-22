f7a6dbb9620a925a4126430057edad707d3138fd
clemlaign <cl.laigneau@gmail.comer>
2017-05-22 10:25:21 +0200

    Ajout des trous 18

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -197,11 +197,29 @@ function($scope, $stateParams, $cordovaGeolocation) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function($scope, $stateParams, $cordovaGeolocation) {
   
-    var options = {timeout: 10000, enableHighAccuracy: true};
+     var options = {timeout: 10000, enableHighAccuracy: true};
+
+   var posTrou1 = {lat : 48.070325 , lng : -1.746956};
+   var posTrou2 = {lat : 48.06957, lng : -1.744317};
+   var posTrou3 = {lat : 48.068987, lng : -1.74351};
+   var posTrou4 = {lat : 48.071311, lng : -1.74638};
+   var posTrou5 = {lat : 48.071182, lng : -1.747844};
+   var posTrou6 = {lat : 48.069631, lng : -1.748327};
+   var posTrou7 = {lat : 48.072125, lng : -1.748555};
+   var posTrou8 = {lat : 48.07246, lng : -1.749323};
+   var posTrou9 = {lat : 48.068859, lng : -1.749293};
+   var item=1;
  
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
  
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+
+    var text="posTrou"+item.toString();
+
+    var pos = eval(text);
+
+    //var distance = google.maps.geometry.spherical.computeDistanceBetween(pos, latLng);
  
     var mapOptions = {
       center: latLng,
@@ -211,14 +229,46 @@ function($scope, $stateParams, $cordovaGeolocation) {
  
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+      var text="posTrou"+item.toString();
+
+      var pos = eval(text);
  
-      var marker = new google.maps.Marker({
+      var marker1 = new google.maps.Marker({
         map: $scope.map,
         animation: google.maps.Animation.DROP,
         position: latLng
       });
-    });
+
+      var marker2 = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: pos
+      });
+
+      var optionsPolylineDistance = {
+          map: $scope.map,
+          path:[pos, latLng],
+          geodesic:true
+        };
+
+      var polylineDistance = new google.maps.Polyline( optionsPolylineDistance );
+
+      $scope.increment = function(){
+        item=item+ 1;
+        if (item==9){item=1;}
+
+        var text="posTrou"+item.toString();
+
+        var pos = eval(text);
+
+        marker2.setPosition(pos);
+        polylineDistance.setPath([pos, latLng]);
+      }
+
+    }); 
  
   }, function(error){
     console.log("Could not get location");
@@ -231,11 +281,38 @@ function($scope, $stateParams, $cordovaGeolocation) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function($scope, $stateParams, $cordovaGeolocation) {
 
-  var options = {timeout: 10000, enableHighAccuracy: true};
+     var options = {timeout: 10000, enableHighAccuracy: true};
+
+   var posTrou1 = {lat : 48.067001, lng : -1.741196};
+   var posTrou2 = {lat : 48.06511045391539, lng : -1.7384228110313416};
+   var posTrou3 = {lat : 48.06191124481533, lng : -1.7402896285057068};
+   var posTrou4 = {lat : 48.06191682208731, lng : -1.7423482239246368};
+   var posTrou5 = {lat : 48.064552761422526, lng : -1.7475530505180359};
+   var posTrou6 = {lat : 48.06192696256268, lng : -1.745017021894455};
+   var posTrou7 = {lat : 48.06523415940042, lng : -1.7449117451906204};
+   var posTrou8 = {lat : 48.06514594323316, lng : -1.7483684420585632};
+   var posTrou9 = {lat : 48.067265118137264, lng : -1.7493769526481628};
+   var posTrou10 = {lat : 48.06686968056081, lng : -1.7427840828895569};
+   var posTrou11 = {lat : 48.06626130932234, lng : -1.7406007647514343};
+   var posTrou12 = {lat : 48.062286439683504, lng : -1.7416200041770935};
+   var posTrou13 = {lat : 48.06523213147448, lng : -1.7402628064155579};
+   var posTrou14 = {lat : 48.061439712251634, lng : -1.7438247799873352};
+   var posTrou15 = {lat : 48.06404119994884, lng : -1.7446012794971466};
+   var posTrou16 = {lat : 48.065678280295536, lng : -1.7409548163414001};
+   var posTrou17 = {lat : 48.06590642296597, lng : -1.7435082793235779};
+   var posTrou18 = {lat : 448.06719971917654, lng : -1.7486514151096344};
+   var item=1;
  
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
  
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+
+    var text="posTrou"+item.toString();
+
+    var pos = eval(text);
+
+    //var distance = google.maps.geometry.spherical.computeDistanceBetween(pos, latLng);
  
     var mapOptions = {
       center: latLng,
@@ -245,14 +322,46 @@ function($scope, $stateParams, $cordovaGeolocation) {
  
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+      var text="posTrou"+item.toString();
+
+      var pos = eval(text);
  
-      var marker = new google.maps.Marker({
+      var marker1 = new google.maps.Marker({
         map: $scope.map,
         animation: google.maps.Animation.DROP,
         position: latLng
       });
-    });
+
+      var marker2 = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: pos
+      });
+
+      var optionsPolylineDistance = {
+          map: $scope.map,
+          path:[pos, latLng],
+          geodesic:true
+        };
+
+      var polylineDistance = new google.maps.Polyline( optionsPolylineDistance );
+
+      $scope.increment = function(){
+        item=item+ 1;
+        if (item==18){item=1;}
+
+        var text="posTrou"+item.toString();
+
+        var pos = eval(text);
+
+        marker2.setPosition(pos);
+        polylineDistance.setPath([pos, latLng]);
+      }
+
+    }); 
  
   }, function(error){
     console.log("Could not get location");
