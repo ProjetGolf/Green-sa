b8485eb6dd0f5276aa59204c4d3d3d63b70ad7fa
MaxCadiou <caioumaxime@gmail.com>
2017-05-27 19:07:56 +0200

    angle deviation = distance

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -537,6 +537,101 @@ function ($scope, $stateParams) {
  
   })
 
+.controller('angleDeDeviation1Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+  $scope.labels =["Parfait", "Trop à droite", "", "", "", "", "Trop à gauche"];
+
+  $scope.data = [
+    [132, 59, 10, 0, 0, 10, 80],
+  
+  ];
+
+
+}])
+
+.controller('angleDeDeviation2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+   $scope.labels =["Parfait", "Trop à droite", "", "", "", "", "Trop à gauche"];
+
+  $scope.data = [
+    [112, 59, 12, 0, 0, 13, 40],
+  
+  ];
+
+
+}])
+
+.controller('angleDeDeviation3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+   $scope.labels =["Parfait", "Trop à droite", "", "", "", "", "Trop à gauche"];
+
+  $scope.data = [
+    [112, 52, 13, 0, 0, 19, 82],
+  
+  ];
+
+
+}])
+
+.controller('distance1Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+  $scope.labels = ['Driver', 'Bois 3', 'Bois 5', 'Fer 5', 'Fer 7', 'Fer 9', 'Putter'];
+  $scope.series = ['Distance moyenne','Distance Max'];
+
+  $scope.data = [
+    [213, 187, 173, 143, 124, 103, 6],
+    [263, 230, 208, 167, 146, 127, 13]
+
+  ];
+
+
+}])
+
+.controller('distance2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+  $scope.labels = ['Driver', 'Bois 3', 'Bois 5', 'Fer 5', 'Fer 7', 'Fer 9', 'Putter'];
+  $scope.series = ['Distance moyenne','Distance Max'];
+
+  $scope.data = [
+    [213, 187, 173, 143, 124, 103, 6],
+    [263, 230, 208, 167, 146, 127, 13]
+
+  ];
+
+}])
+
+.controller('distance3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+  $scope.labels = ['Driver', 'Bois 3', 'Bois 5', 'Fer 5', 'Fer 7', 'Fer 9', 'Putter'];
+  $scope.series = ['Distance moyenne','Distance Max'];
+
+  $scope.data = [
+    [213, 187, 173, 143, 124, 103, 6],
+    [263, 230, 208, 167, 146, 127, 13]
+
+  ];
+
+
+}])
+
 
 
 
